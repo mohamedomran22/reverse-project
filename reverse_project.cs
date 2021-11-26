4d47d147ac20576acc38e08ead92afd8d7d7b151
mohamedomran22 <95096962+mohamedomran22@users.noreply.github.com>
2021-11-26 23:54:27 +0200

    Add files via upload

---
+++ reverse_project.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Console1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<string> MoList = new LinkedList<string>();
+
+            LinkedListNode<string> MoNode = MoList.AddFirst("X1");
+            MoList.AddFirst("x2");
+            MoList.AddFirst("x3");
+            MoList.AddBefore(MokshNode, "X4");
+            MoList.AddLast("Losted");
+
+            Console.WriteLine("Before reversing linked list:  ");
+
+            foreach (var item in MoList)
+            {
+                Console.Write(item + ",");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("After reversing linked list: ");
+
+            foreach (var item in MoList.Reverse())
+            {
+                Console.Write(item + ",");
+            }
+
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
